de84ba5544e8fb557ed3595eb789b00b4483fb00
Navleen <navleenkaurr@gmail.com>
2021-11-10 11:01:33 -0500

    Created exercise schema

--- models/Workout.js
+++ models/Workout.js
@@ -2,46 +2,48 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const ExerciseSchema = new Schema ({
+    type: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    duration: {
+        type: Number,
+        min: 0
+    },
+    weight: {
+        type: Number,
+        trim: true,
+        min: 0
+    },
+    reps: {
+        type: Number,
+        trim: true,
+        min: 0
+    },
+    sets: {
+        type: Number,
+        trim: true,
+        min: 0
+    },
+    distance: {
+        type: Number,
+        min: 0
+    }
+});
+
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
     },
-    exercises: [{
-        type: {
-            type: String,
-            trim: true,
-            required: true
-        },
-        name: {
-            type: String,
-            trim: true,
-            required: true
-        },
-        duration: {
-            type: Number,
-            min: 0
-        },
-        weight: {
-            type: Number,
-            trim: true,
-            min: 0
-        },
-        reps: {
-            type: Number,
-            trim: true,
-            min: 0
-        },
-        sets: {
-            type: Number,
-            trim: true,
-            min: 0
-        },
-        distance: {
-            type: Number,
-            min: 0
-        }
-    }],
+    exercises: [ExerciseSchema],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
