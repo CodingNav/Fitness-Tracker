9265aed7c2579512d2823e13273d8a796a2d51bc
Navleen <navleenkaurr@gmail.com>
2021-11-10 10:53:08 -0500

    Reorganized Workout model

--- models/Workout.js
+++ models/Workout.js
@@ -3,43 +3,45 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    type: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    weight: {
-        type: Number,
-        trim: true,
-        min: 0
-    },
-    sets: {
-        type: Number,
-        trim: true,
-        min: 0
-    },
-    reps: {
-        type: Number,
-        trim: true,
-        min: 0
-    },
-    duration: {
-        type: Number,     
-        min: 0  
-    },
-    distance: {
-        type: Number,
-        min: 0
-    },
-    date_created: {
+    day: {
         type: Date,
         default: Date.now
-    }
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        duration: {
+            type: Number,
+            min: 0
+        },
+        weight: {
+            type: Number,
+            trim: true,
+            min: 0
+        },
+        reps: {
+            type: Number,
+            trim: true,
+            min: 0
+        },
+        sets: {
+            type: Number,
+            trim: true,
+            min: 0
+        },
+        distance: {
+            type: Number,
+            min: 0
+        }
+    }],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
