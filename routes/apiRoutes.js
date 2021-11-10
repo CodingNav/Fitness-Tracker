d5767091ff1f981fdeaa32cb4840009107595b0c
Navleen <navleenkaurr@gmail.com>
2021-11-10 11:54:48 -0500

    Added total duration to workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
 const { Workout } = require('../models');
 
-router.get('/api/workouts', async (req, res) => {
+router.get('/workouts', async (req, res) => {
+    try {
+        // Finds all data in the workout collection
+        const workouts = await Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: "$exercises.duration"
+                    }
+                }
+            }
+        ]);
+        res.status(200).json(workouts);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/workouts/range', async (req, res) => {
     try {
         // Finds all data in the workout collection
         const workouts = await Workout.find({});
@@ -12,21 +31,10 @@ router.get('/api/workouts', async (req, res) => {
     }
 });
 
-router.get('/api/workouts/range', async (req, res) => {
-    try {
-        // Finds all data in the workout collection
-        const workouts = await Workout.find({});
-        res.status(200).json(workouts);
-    }
-    catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-router.put("/api/workouts/:id", async (req, res) => {
+router.put("/workouts/:id", async (req, res) => {
     try {
         // Finds the data with the id from the parameter and changes it to the request body data 
-        const workouts = await Workout.updateOne({_id: req.params.id}, req.body);
+        const workouts = await Workout.updateOne({ _id: req.params.id }, { $push: req.body });
         res.status(200).json(workouts);
     }
     catch (err) {
@@ -34,7 +42,7 @@ router.put("/api/workouts/:id", async (req, res) => {
     }
 });
 
-router.post("/api/workouts", async (req, res) => {
+router.post("/workouts", async (req, res) => {
     try {
         const workouts = await Workout.create(req.body);
         res.status(500).json(workouts);
