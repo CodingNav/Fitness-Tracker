49a7cf179fbd6153a96f5470ee282d613d7db41f
Navleen <navleenkaurr@gmail.com>
2021-11-09 21:05:50 -0500

    Set up Workout model

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    type: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    weight: {
+        type: Number,
+        trim: true,
+        min: 0
+    },
+    sets: {
+        type: Number,
+        trim: true,
+        min: 0
+    },
+    reps: {
+        type: Number,
+        trim: true,
+        min: 0
+    },
+    duration: {
+        type: Number,    
+        default: false,  
+        min: 0  
+    },
+    date_created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
