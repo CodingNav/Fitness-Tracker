2595f0450fa7c51b89cc9fb6c3eacb11c1e69266
Navleen <navleenkaurr@gmail.com>
2021-11-10 12:02:10 -0500

    Fixed add exercise

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -34,7 +34,7 @@ router.get('/workouts/range', async (req, res) => {
 router.put("/workouts/:id", async (req, res) => {
     try {
         // Finds the data with the id from the parameter and changes it to the request body data 
-        const workouts = await Workout.updateOne({ _id: req.params.id }, { $push: req.body });
+        const workouts = await Workout.updateOne({ _id: req.params.id }, { $push: { exercises: req.body }});
         res.status(200).json(workouts);
     }
     catch (err) {
