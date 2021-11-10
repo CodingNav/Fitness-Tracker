bcc8ffa5cf5dd29002d44002f7d4ea90a1f6a146
Navleen <navleenkaurr@gmail.com>
2021-11-10 11:00:18 -0500

    Changed id to _id

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/api/workouts', async (req, res) => {
 router.put("/api/workouts/:id", async (req, res) => {
     try {
         // Finds the data with the id from the parameter and changes it to the request body data 
-        const workouts = await Workout.updateOne({id: req.params.id}, req.body);
+        const workouts = await Workout.updateOne({_id: req.params.id}, req.body);
         res.status(200).json(workouts);
     }
     catch (err) {
