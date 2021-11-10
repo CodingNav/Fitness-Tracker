f05ecb978425e71034b352ee8f38e6600c323b37
Navleen <navleenkaurr@gmail.com>
2021-11-10 11:08:52 -0500

    Created apiRoutes file

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,4 @@
 const router = require('express').Router();
-const { Workout } = require('../models');
-
-router.get('/api/workouts', async (req, res) => {
-    try {
-        // Finds all data in the workout collection
-        const workouts = await Workout.find({});
-        res.status(200).json(workouts);
-    }
-    catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-router.put("/api/workouts/:id", async (req, res) => {
-    try {
-        // Finds the data with the id from the parameter and changes it to the request body data 
-        const workouts = await Workout.updateOne({_id: req.params.id}, req.body);
-        res.status(200).json(workouts);
-    }
-    catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-router.post("/api/workouts", async (req, res) => {
-    try {
-        const workouts = await Workout.create(req.body);
-        res.status(500).json(workouts);
-    }
-    catch (err) {
-        res.status(500).json(err);
-    }
-});
 
 
 module.exports = router;
